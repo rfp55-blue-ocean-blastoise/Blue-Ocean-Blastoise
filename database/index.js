8e0e5af4fe40279615865ec84d9b0d1adb64b248
Aaron Tran <altran32@gmail.com>
2021-10-27 12:09:53 -0700

    enforce eslint rules. remove console.logs. remove unecessary require statements

--- database/index.js
+++ database/index.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
-const config = require("../config.js");
+const mongoose = require('mongoose');
+const config = require('../config.js');
 
 mongoose.connect(
-  `mongodb+srv://brother:${config.mongoPW}@bookbrother.s3z1y.mongodb.net/brotherdb?retryWrites=true&w=majority`
+  `mongodb+srv://brother:${config.mongoPW}@bookbrother.s3z1y.mongodb.net/brotherdb?retryWrites=true&w=majority`,
 );
 
 const db = mongoose.connection;
 
-db.on("error", () => {
-  console.log("mongoose connection error");
+db.on('error', () => {
+  console.log('mongoose connection error');
 });
 
-db.once("open", () => {
-  console.log("mongoose connected successfully");
+db.once('open', () => {
+  console.log('mongoose connected successfully');
 });
 
 const Brothers = mongoose.Schema({
@@ -30,7 +30,7 @@ const Brothers = mongoose.Schema({
   ],
 });
 
-const Brother = mongoose.model("Brother", Brothers);
+const Brother = mongoose.model('Brother', Brothers);
 
 const createUser = async (body) => {
   try {
@@ -53,61 +53,56 @@ const retrieveUserDocument = async (email) => {
 
 const addBookForUser = async (email, book) => {
   try {
-    const { link, title, cfi, remainingText } = book;
-    const result = await Brother.find({ email, "books.title": title });
+    const { title } = book;
+    const result = await Brother.find({ email, 'books.title': title });
     if (result.length === 0) {
       const books = await Brother.findOneAndUpdate(
         { email },
-        { $push: { books: book } }
+        { $push: { books: book } },
       );
       return books;
-    } else {
-      return `Book: ${title} has already been uploaded`;
     }
+    return `Book: ${title} has already been uploaded`;
   } catch (err) {
-    console.log(err, "err from addBookForUser");
     return err;
   }
 };
 
-const updateBookmark = async ({ email, id, cfi, remainingText }) => {
+const updateBookmark = async ({
+  email, id, cfi, remainingText,
+}) => {
   try {
     const result = await Brother.findOneAndUpdate(
-      { email: email, "books._id": id },
-      { $set: { "books.$.cfi": cfi, "books.$.remainingText": remainingText } }
+      { email, 'books._id': id },
+      { $set: { 'books.$.cfi': cfi, 'books.$.remainingText': remainingText } },
     );
     if (result === null) {
       return `Email ${email} does not have the book in their library. Results:${result}`;
-    } else {
-      const split = remainingText.split(" ");
-      return `Email: ${email}
+    }
+    const split = remainingText.split(' ');
+    return `Email: ${email}
       UpdatedCFI: ${cfi}
       remainingText: ${split[0]}, ${split[1]}... ${split[split.length - 1]}`;
-    }
   } catch (err) {
-    console.log(err, "err from updateBookmark");
     return err;
   }
 };
 
-const deleteBook = async ({email, id}) => {
+const deleteBook = async ({ email, id }) => {
   try {
     // const { email, id } = body;
     const results = await Brother.updateOne(
       { email },
-      { $pull: { books: { '_id': id } } }
+      { $pull: { books: { _id: id } } },
     );
-    if (results.modifiedCount === 0){
-      return `modifiedCount = ${results.modifiedCount}`
-    } else {
+    if (results.modifiedCount === 0) {
       return `modifiedCount = ${results.modifiedCount}`;
     }
+    return `modifiedCount = ${results.modifiedCount}`;
   } catch (err) {
-    console.log("err from deleteBook");
     return err;
   }
 };
-
 
 module.exports = {
   db,
--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const path = require("path");
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const fs = require("fs");
-const util = require("util");
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+const util = require('util');
+
 const unlinkFile = util.promisify(fs.unlink);
 const app = express();
 const PORT = 3001;
 const compression = require('compression');
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: 'uploads/' });
 const {
   db,
   createUser,
@@ -18,31 +17,30 @@ const {
   deleteBook,
   addBookForUser,
   updateBookmark,
-} = require("../database/index.js");
-const { uploadFile, listObjectsFromBucket } = require("../aws/s3.js");
+} = require('../database/index.js');
+const { uploadFile, listObjectsFromBucket } = require('../aws/s3.js');
 
-const {aws_bucket_name, aws_bucket_region} = require("../config.js");
+const { aws_bucket_name, aws_bucket_region } = require('../config.js');
+
 const shouldCompress = (req, res) => {
-  if(req.headers['x-no-compression']){
-    return false
+  if (req.headers['x-no-compression']) {
+    return false;
   }
-  return compression.filter(req,res)
-}
+  return compression.filter(req, res);
+};
 
-
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(express.json({ limit: "50mb" }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json({ limit: '50mb' }));
 app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
 );
-app.use(compression({filter: shouldCompress}))
-
+app.use(compression({ filter: shouldCompress }));
 
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${PORT}!`);
 });
 
-app.post("/account", async (req, res) => {
+app.post('/account', async (req, res) => {
   try {
     const result = await createUser(req.body);
     res.status(201).send(result);
@@ -51,10 +49,9 @@ app.post("/account", async (req, res) => {
   }
 });
 
-
-app.get("/account/library", async (req, res) => {
+app.get('/account/library', async (req, res) => {
   try {
-    const email = req.query.email;
+    const { email } = req.query;
     const result = await retrieveUserDocument(email);
     res.status(200).send(result[0].books);
   } catch (err) {
@@ -62,18 +59,17 @@ app.get("/account/library", async (req, res) => {
   }
 });
 
-
-app.post("/account/upload", upload.single("epub"), async (req, res) => {
+app.post('/account/upload', upload.single('epub'), async (req, res) => {
   try {
-    const file = req.file;
+    const { file } = req;
     const { email } = req.body;
     const result = await uploadFile(file, file.originalname);
     await unlinkFile(file.path);
     const book = {
       link: result.Location,
       title: result.Key,
-      cfi: "",
-      remainingText: "",
+      cfi: '',
+      remainingText: '',
     };
     const update = await addBookForUser(email, book);
     res.status(201).send(update);
@@ -82,8 +78,7 @@ app.post("/account/upload", upload.single("epub"), async (req, res) => {
   }
 });
 
-
-app.put("/account/bookmark", async (req, res) => {
+app.put('/account/bookmark', async (req, res) => {
   try {
     const update = await updateBookmark(req.body);
     res.status(201).send(update);
@@ -92,8 +87,7 @@ app.put("/account/bookmark", async (req, res) => {
   }
 });
 
-
-app.delete("/account/library", async (req, res) => {
+app.delete('/account/library', async (req, res) => {
   try {
     const result = await deleteBook(req.body);
     res.status(200).send(result);
@@ -102,36 +96,33 @@ app.delete("/account/library", async (req, res) => {
   }
 });
 
-
-app.get("/library", async (req, res) => {
+app.get('/library', async (req, res) => {
   try {
-    const objects = await listObjectsFromBucket({"Bucket": aws_bucket_name});
-    const list = objects.Contents.map((epub) => {
-      return {
-        Key: epub.Key,
-        Etag: epub.ETag,
-        size: epub.Size,
-        URL: `https://${aws_bucket_name}.s3.${aws_bucket_region}.amazonaws.com/${epub.Key}`,
-      };
-    });
+    const objects = await listObjectsFromBucket({ Bucket: aws_bucket_name });
+    const list = objects.Contents.map((epub) => ({
+      Key: epub.Key,
+      Etag: epub.ETag,
+      size: epub.Size,
+      URL: `https://${aws_bucket_name}.s3.${aws_bucket_region}.amazonaws.com/${epub.Key}`,
+    }));
     res.status(200).send(list);
   } catch (err) {
     res.status(418).send(err);
   }
 });
 
-app.post("/account/library", async (req, res) => {
+app.post('/account/library', async (req, res) => {
   try {
     const { email } = req.body;
     const book = {
       link: req.body.link,
       title: req.body.title,
-      cfi: "",
-      remainingText: "",
+      cfi: '',
+      remainingText: '',
     };
     const update = await addBookForUser(email, book);
     res.status(201).send(update);
   } catch (err) {
     res.status(418).send(err);
   }
-})
+});
